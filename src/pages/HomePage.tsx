c8c4ca7ae26f1bc72eeadefbafef8cc9d45f8a71
Kim Dudenhöfer <kidu2400@student.miun.se>
2026-03-17 15:56:24 +0100

    Läst ut top-spel och senast släppta spel.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,58 @@
 import { Banner } from "../components/Banner"
 import { Link } from "react-router-dom"
+import useGet from "../hooks/useGet"
+import type { GameOverview } from "../types/GameTypes";
+import { GameItem } from "../components/GameItem";
+import { PacmanLoader } from "react-spinners";
 
 export const HomePage = () => {
+
+  const { data: popularGames, error: errorPopularGames, loading: loadingPopularGames } = useGet<GameOverview[]>("https://dt210g-project-backend-hapi.onrender.com/games?sortBy=popularity");
+  const { data: releasedGames, error: errorReleasedGames, loading: loadingReleasedGames } = useGet<GameOverview[]>("https://dt210g-project-backend-hapi.onrender.com/games?sortBy=release-date");
+
   return (
     <div>
       <Banner />
+
+      {/* Top 10 games */}
       <section>
-        <h1>Populära spel</h1>
+        <h1>Top games</h1>
+
+        {/* Listing games */}
+        <div className="gamesContainer">
+          { popularGames && popularGames.slice(0,6).map((game) => (
+              <GameItem key={game.id} game={game} />
+            ))
+          }        
+        </div>
+
+        {/* Spinner */}
+        {loadingPopularGames && <PacmanLoader color="#FEDE5D" className="spinner" />}
+
+        {/* Error */}
+        {errorPopularGames && <p>{ errorPopularGames }</p>}
       </section>
+
+      {/* Latest released games */}
       <section>
-        <h1>Kommande spela</h1>
+        <h1>Latest releases</h1>
+
+        {/* Listing games */}
+        <div className="gamesContainer">
+          { releasedGames && releasedGames.slice(0,6).map((game) => (
+              <GameItem key={game.id} game={game} />
+            ))
+          }
+
+          {/* Spinner */}
+          {loadingReleasedGames && <PacmanLoader color="#FEDE5D" className="spinner" />}
+
+          {/* Error */}
+          {errorReleasedGames && <p>{ errorReleasedGames }</p>}
+        </div>
+
       </section>
-      <Link to ="/games" className="btn">Se alla spel</Link>
+      <Link to ="/games" className="btn">All games</Link>
     </div>
   )
 }
